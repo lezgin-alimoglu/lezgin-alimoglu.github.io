ad1ba67104a70428bfe7c8d0977968fd48855133
Lezgin Alimoğlu <lezgin2003@gmail.com>
2024-11-09 02:26:30 +0300

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
       <p className="text-sm flex justify-center items-center gap-4 mt-2">
         {/* GitHub Links */}
         <a
-          href="https://github.com/Yangwen-li13"
+          href="https://github.com/lezgin-alimoglu"
           className="text-blue-400 hover:underline flex items-center gap-1"
           target="_blank"
           rel="noopener noreferrer"
