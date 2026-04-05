868802917b4f565a10c37333b6b75c45f448b3c5
lezgin-alimoglu <lezgin2003@gmail.com>
2026-04-05 21:47:35 +0300

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',      // Statik export modu
+  images: {
+    unoptimized: true,   // GitHub Pages'de resim optimizasyonu çalışmaz
+  },
+  // EĞER siten lezgin.github.io/portfolio gibi bir alt klasördeyse:
+  // basePath: '/portfolio', 
 };
 
 export default nextConfig;
